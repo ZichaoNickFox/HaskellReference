b57eda44df90e8f390b0ae7e51751631669ece2d
zichaonickfox <zichaonickfox@gmail.com>
2024-02-19 23:55:25 +0800

    Katip

--- Package/KatipSpec.hs
+++ Package/KatipSpec.hs
@@ -1,9 +1,30 @@
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell   #-}
 
 module Package.KatipSpec where
 
+import           Control.Exception
+import           Data.Text
 import           Katip
+import           System.IO
 import           Test.Hspec
+
+-- CMap
+-- https://cmapcloud.ihmc.us/cmaps/myCmaps.html
+-- Home -> Haskell -> Katip
+
+logSomething :: (KatipContext m) => m ()
+logSomething = do
+  $(logTM) InfoS "Log in no namespace"
+  katipAddNamespace "ns1" $
+    $(logTM) InfoS "Log in ns1"
+  katipAddNamespace "ns2" $ do
+    $(logTM) WarningS "Log in ns2"
+    katipAddNamespace "ns3" $
+      katipAddContext (sl "userId" True) $ do
+        $(logTM) InfoS "Log in ns2.ns3 with userId context"
+        katipAddContext (sl "country" (pack "China")) $
+          $(logTM) InfoS "Log in ns2.ns3 with userId and country context"
 
 runKatip :: IO ()
 runKatip = withKatip $ \le -> runKatipContextT le () mempty logSomething
@@ -13,12 +34,12 @@ withKatip = bracket createLogEnv closeScribes
   where
     createLogEnv = do
       logEnv <- initLogEnv "HAuth" "dev"
-      stdoutScribe <- mkHandleScribe ColorIfTerminal stdout InfoS V2
+      stdoutScribe <- mkHandleScribe ColorIfTerminal stdout (permitItem InfoS) V2
       registerScribe "stdout" stdoutScribe defaultScribeSettings logEnv
 
 spec :: SpecWith ()
 spec = do
-
+  it "runKatip" $ runKatip
 
 main :: IO ()
 main = hspec spec
